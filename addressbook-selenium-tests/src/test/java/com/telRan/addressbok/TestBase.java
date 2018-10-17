7fe5d49be4dfb2ee415999723f02e40ebf01f488
Maria <troubniakova@gmail.com>
2018-10-17 18:26:44 +0300

    homework

--- addressbook-selenium-tests/src/test/java/com/telRan/addressbok/TestBase.java
+++ addressbook-selenium-tests/src/test/java/com/telRan/addressbok/TestBase.java
@@ -93,7 +93,6 @@ public class TestBase {
         type(By.name("address"), adress);
         type(By.name("mobile"), mobile);
         type(By.name("email"), email);
-
     }
 
     public void selectContact() {
